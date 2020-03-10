2a6c83402b8b50ec042e30ec62d911699d6c16c8
Hossein2x <hrakbari77@gmail.com>
2020-03-10 21:42:36 +0330

    v-model set

--- src/App.vue
+++ src/App.vue
@@ -4,8 +4,13 @@
             <div class="col-xs-12 col-sm-8 col-sm-offset-2 col-md-6 col-md-offset-3">
                 <div class="form-group">
                     <label>UserName</label>
-                    <input class ="form-control" type="text">
+                    <input class ="form-control" type="text" v-model = "username">
                 </div>
+                <div class="form-group">
+                    <label>mail</label>
+                    <input class ="form-control" type="text" v-model = "mail">
+                </div>
+                <button class="btn btn-primary">Submit</button>
             </div>
         </div>
     </div>
